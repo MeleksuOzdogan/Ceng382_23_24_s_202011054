021a4296365e5604e615b33a1f36616f3ed2949c
MeleksuOzdogan <mlksu.ozd@gmail.com>
2024-05-31 02:49:13 +0300

    Reservation modelis updated

---
+++ ReservationApp/Data/Migrations/20240530234743_ReservationTableUpdate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReservationTableUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReserverName",
+                table: "Reservations",
+                newName: "UpdatedBy");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReserverId",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "ReserverId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Reservations");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedBy",
+                table: "Reservations",
+                newName: "ReserverName");
+        }
+    }
+}
--- ReservationApp/Models/Reservetion.cs
+++ ReservationApp/Models/Reservetion.cs
@@ -2,8 +2,15 @@ public class Reservation
 {
     public string ReservationId { get; set; }
     public DateTime DateTime { get; set; }
-    public string ReserverName { get; set; }
+
+    public string ReserverId { get; set; }
 
     public string RoomId { get; set; }
     public Room Room { get; set; }
+
+
+    public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string UpdatedBy { get; set; }
 }
