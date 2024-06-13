a3a6f3fa46316eea03c94db337714fa30b1ff3bb
Yazgı Kavukçu <65962091+KodMachine@users.noreply.github.com>
2024-06-14 02:05:19 +0300

    not

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f666beb2a35a22ed5b34e0c07021648ef9ed083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b66a8fd97926bb4bf5d66004039443ddd4900b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
